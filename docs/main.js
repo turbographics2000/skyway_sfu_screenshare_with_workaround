033c2dc741555766023769b36e4b3ab6d1f8bd36
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-07-26 22:57:47 +0900

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,37 @@
+const myId = (new MediaStream).id;
+console.log(`myId:${myId}`);
+function appendVideo(stream) {
+    const video = document.createElement('video');
+    video.srcObject = stream;
+    document.body.appendChild(video);
+    video.play();
+}
+chrome.runtime.sendMessage('eiceogpklagmibnoccdincfglccflknk', { cap: true }, async streamId => {
+    let stream = null;
+    try {
+        stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                mandatory: {
+                    chromeMediaSource: 'desktop',
+                    chromeMediaSourceId: streamId
+                }
+            }
+        });
+        appendVideo(stream);
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+    console.log(`streamId:${stream.id}`);
+    const peer = new Peer(myId, {
+        key: '01099bd8-1083-4c33-ba9b-564a1377e901'
+    });
+    peer.on('open', id => {
+        myIdDisp.textContent = id;
+        const room = peer.joinRoom('hoge_fuga_piyo_sfu', { mode: 'sfu', stream });
+        room.on('stream', stream => {
+            console.log(`room on stream peerId:${stream.peerId}`);
+            appendVideo(stream);
+        });
+    });
+});

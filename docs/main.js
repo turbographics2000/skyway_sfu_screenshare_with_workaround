1f63c093d30ff9ce69ab0cb2011bf4bae721e53a
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-07-26 23:04:29 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,9 @@
 const myId = (new MediaStream).id;
 console.log(`myId:${myId}`);
+const skywayApiKey = '01099bd8-1083-4c33-ba9b-564a1377e901';
+const roomName = 'hoge_fuga_piyo_sfu';
+const roomMode = 'sfu';
+
 function appendVideo(stream) {
     const video = document.createElement('video');
     video.srcObject = stream;
@@ -24,11 +28,18 @@ chrome.runtime.sendMessage('eiceogpklagmibnoccdincfglccflknk', { cap: true }, as
     }
     console.log(`streamId:${stream.id}`);
     const peer = new Peer(myId, {
-        key: '01099bd8-1083-4c33-ba9b-564a1377e901'
+        key: skywayApiKey
     });
     peer.on('open', id => {
         myIdDisp.textContent = id;
-        const room = peer.joinRoom('hoge_fuga_piyo_sfu', { mode: 'sfu', stream });
+        const room = peer.joinRoom(roomName, { mode: roomMode, stream, videoBandwidth: 2048 });
+        room.on('open', _ => {
+            const dummyPeer = new Peer({ key: skywayApiKey });
+            dummyPeer.on('open', _ => {
+                const dummyRoom = dummyPeer.joinRoom(roomName, { mode: roomMode });
+                dummyRoom.on('open', _ => dummyRoom.close());
+                dummyRoom.on('close', _ => dummyPeer.destroy());
+            });
         room.on('stream', stream => {
             console.log(`room on stream peerId:${stream.peerId}`);
             appendVideo(stream);
